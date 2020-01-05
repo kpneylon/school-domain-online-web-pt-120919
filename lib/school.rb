01ac4cbfdb213e682aaafddb956ddc5a674d986a
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-05 18:36:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,12 @@ class School
     roster[level] << student
   end  
   
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
+  
 end  
