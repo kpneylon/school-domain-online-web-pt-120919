092710cdbebbf4f9c9b8e1ceb89c557692aa3667
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-05 18:40:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,10 +21,11 @@ class School
   end 
   
   def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
+    nu_hash = {}
+    roster.each do |x, y| 
+      nu_hash[x] = y.sort 
+    end 
   nu_hash
-  
+
+
 end  
